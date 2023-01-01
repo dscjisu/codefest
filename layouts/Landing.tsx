a4f97f4ed286ee36dfa7be6abe71963bab2880cb
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-01 23:10:23 +0530

    perf: landingg

--- layouts/Landing.tsx
+++ layouts/Landing.tsx
@@ -1,53 +1,37 @@
 import Image from "next/image";
-import { useEffect } from "react";
-
+import Devfolio from "../components/Devfolio";
 
 
 function Landing() {
 
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://apply.devfolio.co/v2/sdk.js';
-        script.async = true;
-        script.defer = true;
-        document.body.appendChild(script);
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
 
 
     return (
         <>
-            <div className=" min-w-screen lg:container mx-auto lg:p-10 pt-10">
+            <div id="Home" className="min-w-screen container mx-auto lg:p-10 pt-10">
                 <div className="flex flex-row lg:gap-2 items-center justify-center">
                     <div className="container mx-auto">
-                        <div className="flex flex-row justify-center items-center h-full">
-                            <div className="flex flex-col gap-1 px-5 basis-1/2">
+                        <div className="flex flex-row justify-center  items-center h-full">
+                            <div className="flex flex-col gap-1 lg:px-5 px-2 lg:basis-1/2">
                                 <h1 className="lg:text-6xl text-3xl font-mono font-bold text-center lg:text-left pb-4">CodeFest 2023</h1>
-                                <p className="font-semibold text-lg text-left py-2">
+                                <p className="font-semibold text-lg text-left py-2 px-2">
                                     Unleash your inner coder/Techie and join the revolution
                                 </p>
-                                <p className="font-semibold text-lg text-left py-2">
+                                <p className="font-semibold text-lg text-left py-2 px-2">
                                     <span className="font-bold">Date:</span> <span className="font-thin">To be announced soon</span>
                                 </p>
-                                <p>
-                                    <span className="font-bold">Theme:</span> <span className="font-thin"></span>
-                                    <ul className="ml-5 text-gray-400">
-                                        <li className="font-thin list-disc">Prizes worth <span className="font-bold">7.5 Lakhs</span></li>
-                                        <li className="font-thin list-disc">Hackathon</li>
-                                        <li className="font-thin list-disc">Workshop</li>
-                                        <li className="font-thin list-disc">Tech Talks</li>
-                                        <li className="font-thin list-disc">Networking</li>
-
+                                <span className="px-2">
+                                    <span className="font-bold">Theme:</span> <span className="font-thin">To be announced soon</span>
+                                    <ul className="ml-5 text-gray-400 pt-5">
+                                        <li className="font-thin list-disc">Prizes worth : <span className="font-bold">9+ Lakhs</span></li>
+                                        <li className="font-thin list-disc">Hackathon : <span className="font-bold">3+ Theme</span></li>
+                                        <li className="font-thin list-disc">Workshop : <span className="font-bold">5+ Tools</span></li>
+                                        <li className="font-thin list-disc">Tech Talks </li>
+                                        <li className="font-thin list-disc">Networking </li>
                                     </ul>
-                                </p>
+                                </span>
                                 <div className="flex flex-row gap-2 pt-10">
-                                    <div
-                                        className="apply-button rounded-3xl"
-                                        data-hackathon-slug="hacknitr4"
-                                        data-button-theme="dark-inverted"
-                                    />
+                                    <Devfolio />
                                 </div>
                             </div>
                             <div className="hidden lg:flex justify-center items-center w-1/2">
@@ -61,10 +45,14 @@ function Landing() {
                                 />
                             </div>
                         </div>
-                        <div>
-
-                        </div>
                     </div>
+                </div>
+                <div>
+                    <div className="1"></div>
+                    <div className="1"></div>
+                    <div className="1"></div>
+                    <div className="1"></div>
+                    <div className="1"></div>
                 </div>
             </div>
         </>
