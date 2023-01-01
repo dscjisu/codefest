c21c90e2f012b65875c633618e1eb30c20ce56f0
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-02 00:32:10 +0530

    build: sponsor yet

--- layouts/Sponsor.tsx
+++ layouts/Sponsor.tsx
@@ -1,7 +1,32 @@
 
 function Sponsor() {
     return (
-        <div>Sponsor</div>
+        <>
+            <section id="Sponsors" className="container mx-auto lg:p-10 p-2">
+                <div className="relative flex flex-col px-5">
+                    <h1 className="lg:text-6xl text-3xl font-mono font-bold text-center pb-4 backdrop:bg-yellow-400">Sponsors</h1>
+                    <div className="pb-10" />
+                    <div className='grid grid-cols-4 gap-4 lg:px-36 justify-items-center place-content-center'>
+                    </div>
+                    <p className="font-semibold text-gray-600 lg:text-center  lg:px-40 text-clip text-lg">
+                        We are looking for sponsors to help us make this event a success. If you are interested in sponsoring this event, please contact us at <a href="mailto:dscjisu@gmail.com"
+                            className="text-blue-500">
+                            <span className="text-pink-600">
+                                Mail
+                            </span>
+                        </a>
+                    </p>
+                </div>
+                <h1 className="text-center">
+                    To be Announced
+                    <br />
+
+                </h1>
+                <div
+                    className="lg:pb-20 pb-2"
+                />
+            </section>
+        </>
     )
 }
 
