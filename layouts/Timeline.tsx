c53b9ff47d676e217d876b163d57974623fc8a65
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-02 00:32:32 +0530

    build: timeline section

--- layouts/Timeline.tsx
+++ layouts/Timeline.tsx
@@ -1,12 +1,26 @@
 
-
 function Timeline() {
-    return (
-        <div>
-            <h1>Timeline</h1>
-            <h2>Starting Soon </h2>
 
-        </div>
+    return (
+        <section id="Timeline" className="container mx-auto lg:p-10 p-2">
+            <div className="relative flex flex-col px-5">
+                <h1 className="lg:text-6xl text-3xl font-mono font-bold text-center pb-4 backdrop:bg-yellow-400">Timeline</h1>
+                <div className="pb-10" />
+                <div className='grid grid-cols-4 gap-4 lg:px-36 justify-items-center place-content-center'>
+                </div>
+                <p className="font-semibold text-gray-600 lg:text-center  lg:px-40 text-clip text-lg">
+                    Wait.....
+                </p>
+            </div>
+            <h1 className="text-center">
+                To be Announced
+                <br />
+
+            </h1>
+            <div
+                className="lg:pb-20 pb-2"
+            />
+        </section>
     )
 }
 
