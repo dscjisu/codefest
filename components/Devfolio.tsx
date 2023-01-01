559eaa5a0dba178d90f78f7a53e8aa2015c3ce6e
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-01 20:22:51 +0530

    build: devfolio

---
+++ components/Devfolio.tsx
@@ -0,0 +1,24 @@
+
+
+
+function Devfolio() {
+    return (
+        <div className="">
+            {/* <div
+                className="apply-button rounded-2xl bg-black"
+                data-hackathon-slug="dfvdv"
+                data-button-theme="light"
+            />
+
+
+            <h1 className="text-white">Devfolio Link</h1> */}
+            <button className="px-5 py-2 text-black capitalize text-justify font-bold font-mono cursor-progress rounded-lg bg-yellow-400 ">
+                <h1>
+                    Registration Opening Soon 😃
+                </h1>
+            </button>
+        </div>
+    )
+}
+
+export default Devfolio
