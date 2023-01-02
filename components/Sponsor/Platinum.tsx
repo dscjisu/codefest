1518388401a08f4aeae1fd8f6c71cfca9c7e6400
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-02 23:15:07 +0530

    build: platnium spons

---
+++ components/Sponsor/Platinum.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+
+import PlatinumSponsor from '../../content/Sponsors/Platinum.json'
+
+function Platinum() {
+    return (
+        <div className="lg:mx-10 flex justify-center flex-col gap-10  py-5  rounded-2xl shadow-inner shadow-gray-500 drop-shadow-2xl Platinum_sponsor ">
+            <h1
+                className='text-3xl text-center font-bold text-emerald-600'
+            >
+                PLATINUM SPONSORS
+            </h1>
+            <div className="grid lg:grid-cols-3 px-10  gap-10">
+                {
+                    PlatinumSponsor?.map((item, index) => {
+                        return (
+                            <div className="flex justify-center items-center  col-span-1" key={index}>
+                                <Image className="rounded-lg bg-cover"
+                                    src={item?.image}
+                                    alt={item?.name}
+                                    width={250} height={250} />
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default Platinum
